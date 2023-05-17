8c74974d52776587471b4fb85da75ccc2d6108b7
Ivan Valera <ivanvaleraa@gmail.com>
2023-05-17 00:51:53 -0400

    organizando componente eslint

--- src/Date.jsx
+++ src/Date.jsx
@@ -1,11 +1,13 @@
-function Date(props){
-    return (
-    <div className="date--content">
-        <input className="date--input" 
-        onChange={e=> props.updateDate(e.target.value)} 
+import React from 'react'
+
+function Date (props) {
+  return (
+  <div className="date--content">
+        <input className="date--input"
+        onChange={e => props.updateDate(e.target.value)}
         type="date" />
     </div>
-    )
+  )
 }
 
 export default Date
